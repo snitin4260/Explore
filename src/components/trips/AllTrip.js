df710f3b54e703a0d735be9134a1ff366663ed81
nitin <snitin8994@gmail.com>
2019-09-09 22:34:07 +0530

    Change all trip page design

--- src/components/trips/AllTrip.js
+++ src/components/trips/AllTrip.js
@@ -1,48 +1,83 @@
-import React from 'react'
-import Header from '../header/Header'
-import getTripsData from '../../actions/getTripsData'
-import BackgroundContainer from '../BackgroundContainer/BackgroundContainer'
-import { connect } from 'react-redux'
-import styles from './AllTrip.module.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+import Header from "../header/Header";
+import getTripsData from "../../actions/getTripsData";
+import BackgroundContainer from "../BackgroundContainer/BackgroundContainer";
+import styles from "./AllTrip.module.css";
 
 const mapStateToProps = state => {
   return {
     tripsData: state.tripNamesData
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
     getTripsData: () => dispatch(getTripsData())
-  }
-}
+  };
+};
+
+const ListContainer = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const List = styled.li`
+  display: inline-block;
+  font-size: 1.6rem;
+  max-width: 450px;
+  width: 100%;
+  overflow-wrap: break-word;
+  margin-bottom: 2rem;
+  text-align: center;
+  font-family: "Roboto", sans-serif;
+  background: white;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+  border-radius: 3px;
+  /* padding: 1rem; */
+`;
+
+const Title = styled.h2`
+margin-bottom: 2.5rem;
+font-family: "Roboto",sans-serif;
+font-size: 3rem;
+color: #2874f0;
+`;
 
 class AllTrip extends React.Component {
-  componentDidMount () {
-    this.props.getTripsData()
+  componentDidMount() {
+    this.props.getTripsData();
   }
 
-  render () {
-    const { trips, isLoading, error } = this.props.tripsData
-    console.log(trips)
+  render() {
+    const { trips, isLoading, error } = this.props.tripsData;
+    console.log(trips);
     return (
       <>
         <Header />
         <BackgroundContainer>
           <div className={styles.inner_container}>
             {!isLoading ? (
-              <>
+              <ListContainer>
+                <Title>All Trips</Title>
                 {trips.map(data => {
                   return (
-                    <div className={styles.content} key={data._id}>
-                      <Link className={styles.link} to={`/trip/itinerary/${data._id}`}>
+                    <List className={styles.content} key={data._id}>
+                      <Link
+                        className={styles.link}
+                        to={`/trip/itinerary/${data._id}`}
+                      >
                         {data.tripName} ({data.createdAt})
                       </Link>
-                    </div>
-                  )
+                    </List>
+                  );
                 })}
-              </>
+              </ListContainer>
             ) : (
               <div>
                 <div className={`loadingDiv ${styles.block}`} />
@@ -54,11 +89,11 @@ class AllTrip extends React.Component {
           </div>
         </BackgroundContainer>
       </>
-    )
+    );
   }
 }
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(AllTrip)
+)(AllTrip);
