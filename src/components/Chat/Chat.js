b1a67dd3105a5e354f8a5486f7316f60ed3d2951
nitin <snitin8994@gmail.com>
2019-09-10 19:12:11 +0530

    Add location in request payload in edit itinerary

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import {connect} from 'react-redux'
 
-import UserDashboard from "../UserdashBoard/UserDashBoard"
-
+import UserDashboard from "../UserdashBoard/UserDashBoard";
+import socketIOClient from "socket.io-client";
 
 class Chat extends React.Component {
-    render() {
-        return (
-            <UserDashboard selected="chat">
-                Hi
-            </UserDashboard>
-        )
-    }
+  componentDidMount() {
+    // const { id } = this.props.match.params;
+    // if (!this.props.itinerary[id]) {
+    //   this.props.setTripObject(id);
+    //   this.props.setItineraryObject(id);
+    //   const { getItineraryData } = this.props;
+    //   getItineraryData(id);
+    // }
+  }
+  render() {
+    return <UserDashboard selected="chat"></UserDashboard>;
+  }
 }
 
-export default Chat
+export default connect()(Chat);
